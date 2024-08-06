8b51cb8316cb80162f15f5e924d28d87dda95af4
AnaBeatrizRomera <ana.romera@germinare.org.br>
2024-08-06 12:09:04 -0300

    Subindo alterações

---
+++ index.js
@@ -0,0 +1,9 @@
+// console.log('Hello World!')
+
+// //Common JS
+// const matematica = require('./matematica')
+// console.log(matematica.soma(5,3));//8
+// console.log(matematica.subtracao(5,3));//2
+import { soma, subtracao } from './matematica.js';
+console.log(soma(5,3));//8
+console.log(subtracao(5,3));//2
---
+++ matematica.js
@@ -0,0 +1,4 @@
+export const soma = (a, b) => a + b;
+export const subtracao = (a, b) => a - b;
+
+// module.exports = { soma, subtracao };
